657d7bb226bfcc1472c264c452ae289c0d78e019
wkwkrnht <wkwkrnht@gmail.com>
2016-05-01 15:49:19 +0900

    fix

--- footer.php
+++ footer.php
@@ -2,9 +2,6 @@
 	<div id="movetop">∧</div>
 	<script>jQuery('#movetop').click(function(){jQuery('body,html').animate({scrollTop:0},500);});</script>
 </div>
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/styles/default.min.css">
-<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/highlight.min.js"></script>
 <?php wp_footer();?>
 </body>
 </html>
