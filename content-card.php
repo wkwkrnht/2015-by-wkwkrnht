e70a45b73c2590abb9a61f4154ba7bf0bf15b16d
wkwkrnht <wkwkrnht@gmail.com>
2016-05-01 21:07:39 +0900

    fix

--- content-card.php
+++ content-card.php
@@ -1,4 +1,4 @@
-<style>.card-list{border-radius:8px;}.card-info{background-color:#f7f7f7;color:#707070;margin-bottom:5px;}.card-thumb{max-width:825px;max-height:510px;}.card-title{font-size:40px;text-align:center;}</style>
+<style>.card-list{border-radius:8px;}.card-info{background-color:#f7f7f7;color:#707070;margin-bottom:5px;}.card-thumb{max-width:825px;max-height:510px;}.card-title{font-size:40px;text-align:center;}@media screen and (max-width:55em){.card-list{margin-bottom:15px;}.card-thumb{max-width:825px;max-height:510px;margin-bottom:5px;}.card-title{font-size:27px;}.card-meta{font-size:14px;}}</style>
 <article id="post-<?php the_ID();?>" <?php post_class();?>>
 	<section class="card-list">
 		<div class="card-thumb"><?php twentyfifteen_post_thumbnail();?></div>
