fa15c28a98c04cfda02dc9b680bb2e845263ed92
wkwkrnht <wkwkrnht@gmail.com>
2016-05-03 22:13:06 +0900

    fix

--- content.php
+++ content.php
@@ -19,5 +19,5 @@
 	</section>
 	<?php get_sidebar('8');?>
 	<div id="movetop">∧</div>
-	<?php if(!comments_open()):echo?><script>jQuery('#movetop').click(function(){jQuery('body,html').animate({scrollTop:0},500);});</script><?php ;endif;?>
+	<?php if(!comments_open()):echo"<script>jQuery('#movetop').click(function(){jQuery('body,html').animate({scrollTop:0},500);});</script>";endif;?>
 </article>
