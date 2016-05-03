76ed72a697bd50583843befc136c2853811e27fa
wkwkrnht <wkwkrnht@gmail.com>
2016-05-03 17:31:34 +0900

    fix

--- archive.php
+++ archive.php
@@ -10,11 +10,7 @@
 				<?php the_archive_title('<h1 class="page-title">','</h1>');
 					  the_archive_description('<div class="taxonomy-description">','</div>');?>
 				</header>
-				<?php while(have_posts()):the_post();
-				if(is_page()||is_single()):
-					get_template_part('content',get_post_format());echo'<div id="movetop">∧</div><script>document.getElementById("movetop").click(function(){jQuery("body,html").animate({scrollTop:0},500);});</script>';
-				else:get_template_part('content-card',get_post_format());endif;endwhile;
-				the_posts_pagination(array('prev_text'=>__('Previous page','twentyfifteen'),'next_text'=>__('Next page','twentyfifteen'),'before_page_number' =>'<span class="meta-nav screen-reader-text">' . __('Page','twentyfifteen') . ' </span>',));
+				<?php while(have_posts()):the_post();if(is_page()||is_single()):get_template_part('content',get_post_format());else:get_template_part('content-card',get_post_format());endif;endwhile;the_posts_pagination(array('prev_text'=>__('Previous page','twentyfifteen'),'next_text'=>__('Next page','twentyfifteen'),'before_page_number' =>'<span class="meta-nav screen-reader-text">' . __('Page','twentyfifteen') . ' </span>',));
 			else:get_template_part('content','none');endif;?>
 			</main>
 		</section>
