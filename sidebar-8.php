5730fdd77ba7f4c0894dd05d9d960faf402fc40f
wkwkrnht <wkwkrnht@gmail.com>
2016-05-03 17:10:55 +0900

    fix

--- sidebar-8.php
+++ sidebar-8.php
@@ -1,6 +1,4 @@
 <footer class="entry-footer">
-	<div id="movetop">∧</div>
-	<script>jQuery('#movetop').click(function(){jQuery('body,html').animate({scrollTop:0},500);});</script>
 	<h2><?php echo get_option('entryfooter_txt')?></h2>
 	<?php if(is_active_sidebar('8')):
 		dynamic_sidebar('8');
