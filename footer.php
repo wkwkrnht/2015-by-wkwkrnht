b29716318249213a67851da6c056420e7da3a2d9
wkwkrnht <wkwkrnht@gmail.com>
2016-05-07 00:27:10 +0900

    fix

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 </div>
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/styles/default.min.css">
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/highlight.min.js"></script>
-<script src="<?php echo home_uri();?>/wp-content/themes/2015-by-wkwkrnht/js/lity.min.js"></script>
+<script src="<?php echo home_url();?>/wp-content/themes/2015-by-wkwkrnht/js/lity.min.js"></script>
 <script> 
     (function(){var doc=document;var wpCss=doc.getElementsByClassName('wpcss');var wpCssL=wpCss.length;for(i=0;i<wpCssL;i++){var wpStyle=doc.createElement('style');wpStyle.textContent=wpCss[i].textContent.replace(/\s{2,}/g,"");doc.head.appendChild(wpStyle);}})()
 </script>
