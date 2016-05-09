5fdb3c7246d4c356e977ff615e599752547763d2
wkwkrnht <wkwkrnht@gmail.com>
2016-05-09 21:52:39 +0900

    Revert "test"
    This reverts commit 9e982b0f848af3265b81ed63a3320d590cdc6672.

--- footer.php
+++ footer.php
@@ -5,8 +5,6 @@
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/highlight.min.js"></script>
 <script src="<?php echo home_url();?>/wp-content/themes/2015-by-wkwkrnht/js/lity.min.js"></script>
 <script>
-	jQuery(document.ready(function(){jQuery('code').each(function(i,block){hljs.highlightBlock(block);});}));
-	jQuery(function tableData(){var index='';var headTxt='';$('.tableBlock02 table').each(function(){$(this).find('thead tr th').each(function(){index=$(this).index()-1;headTxt=$(this).text();console.log(headTxt)$(this).parents('table').find('tbody tr').each(function(){$(this).find('td').eq(index).attr('data-th',headTxt);});});});tableData();});
 	(function($){$(document).on('keydown.twentyfifteen',function(e){var url=false;if(e.which===37){url=$('.nav-previous a').attr('href');}else if(e.which===39){url=$('.nav-next a').attr('href');}if(url&&(!$('textarea,input').is(':focus'))){window.location=url;}});})(jQuery);
     (function(){var doc=document;var wpCss=doc.getElementsByClassName('wpcss');var wpCssL=wpCss.length;for(i=0;i<wpCssL;i++){var wpStyle=doc.createElement('style');wpStyle.textContent=wpCss[i].textContent.replace(/\s{2,}/g,"");doc.head.appendChild(wpStyle);}})()
 </script>
