1cb7614c195281da14a1ffbba7f9657356dba78f
wkwkrnht <wkwkrnht@gmail.com>
2016-05-09 21:53:50 +0900

    Revert "Revert "fix""
    This reverts commit f1ce13af1f4d3c281ef7a79840df4ed023376bd2.

--- header.php
+++ header.php
@@ -102,7 +102,7 @@
 		<header id="masthead" class="site-header" role="banner">
 			<div class="site-branding">
 				<?php twentyfifteen_the_custom_logo();
-				if(is_front_page()&&is_home()):echo'<h1 class="site-title"><a href="' . esc_url(home_url('/')) . '" rel="home">' . bloginfo('name') . '</a></h1>';else:echo'<p class="site-title"><a href="' . esc_url(home_url('/')) . '" rel="home">' . bloginfo('name') . '</a></p>';endif;
+				if(is_front_page()&&is_home()):echo'<h1 class="site-title"><a href="' . esc_url(home_url('/')) . '" rel="home">' . bloginfo('name') . '</a></h1>';else:echo'<a href="' . esc_url(home_url('/')) . '" class="site-title" rel="home">' . bloginfo('name') . '</a>';endif;
 				$description=get_bloginfo('description','display');if($description||is_customize_preview()):echo'<p class="site-description">' . $description . '</p>';endif;?>
 			</div>
 		</header>
